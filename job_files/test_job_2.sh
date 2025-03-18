bc2935faeb6d27cccf84c422dc145e31b23e3293
Muye Xiao <muye.xiao19@imperial.ac.uk>
2025-03-18 14:33:11 +0000

    test more cores

---
+++ job_files/test_job_2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=32:mem=4gb
+#PBS -l walltime=00:20:00
+#PBS -N simba_test
+
+module purge
+module load miniforge/3
+
+eval "$(~/miniforge3/bin/conda shell.bash hook)"
+conda activate simba_env
+
+cd $HOME/auto_react_mech_construct
+
+python main.py
+
